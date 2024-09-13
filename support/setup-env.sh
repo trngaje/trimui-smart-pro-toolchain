4689a12713cd324be0bfad65fc53271be79a0d44
Aníbal Deboni Neto <anibaldeboni@gmail.com>
2024-09-13 10:14:28 -0300

    build image

--- support/setup-env.sh
+++
@@ -1,3 +0,0 @@
-#export PATH="/opt/aarch64-linux-gnu-7.5.0-linaro/usr/bin:${PATH}:/opt/aarch64-linux-gnu-7.5.0-linaro/usr/arm-linux-gnueabihf/sysroot/bin"
-export CROSS_COMPILE=/opt/aarch64-linux-gnu-7.5.0-linaro/bin/aarch64-linux-gnu-
-export PREFIX=/opt/aarch64-linux-gnu-7.5.0-linaro/sysroot/usr
